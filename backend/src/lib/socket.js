1c7395c7b6dafe7ed35abafe3fb9af6eb175c991
Aravindhan-K97 <aravindhan2825@gmail.com>
2025-04-12 20:50:54 +0530

    commit

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -9,8 +9,7 @@ const io = new Server(server, {
   cors: {
     origin: ["http://localhost:5173"],
     credentials: true,
-    methods: ["GET", "POST"],
-  }
+  },
 });
 
 export function getReceiverSocketId(userId) {
